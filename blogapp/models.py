55291a8ed57a837e0a095e481c948b4147f823b5
yinpeng <jianglixiaobai@sina.com>
2016-05-18 14:51:03 +0800

    models.py 完善 添加 书籍介绍 电影介绍 语言介绍

--- blogapp/models.py
+++ blogapp/models.py
@@ -8,12 +8,12 @@ class Publishers(models.Model):
     '''
     出版社集
     '''
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    state_province = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    website = models.URLField()
+    name = models.CharField(max_length=50,verbose_name=u'出版社')
+    address = models.CharField(max_length=100,verbose_name=u'地址')
+    city = models.CharField(max_length=100,verbose_name=u'所在城市')
+    state_province = models.CharField(max_length=50,verbose_name=u'所在省份')
+    country = models.CharField(max_length=50,verbose_name=u'国家')
+    website = models.URLField(verbose_name=u'网址')
 
     def __str__(self):
         return self.name
@@ -25,8 +25,8 @@ class Authors(models.Model):
     '''
     书籍作者集
     '''
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=50,verbose_name=u'姓')
+    last_name = models.CharField(max_length=50,verbose_name=u'名')
     author_img = models.ImageField(upload_to='photos',verbose_name=u'作者写真')   #添加作者写真
     author_info = models.TextField(verbose_name=u'作者简介')
     email = models.EmailField(blank=True,verbose_name='e-mail')
@@ -42,6 +42,7 @@ class Books(models.Model):
     title = models.CharField(max_length=100,verbose_name=u'书名')
     book_img = models.ImageField(upload_to='photos',verbose_name=u'封面')   #添加书封面字段 2016/5/18
     author = models.ManyToManyField(Authors,verbose_name=u'作者')
+    book_intro = models.TextField(verbose_name=u'书籍简介')             #添加书籍简介  2016/5/18
     publisher = models.ForeignKey(Publishers,verbose_name=u'出版社')
     book_pub_date = models.DateTimeField(blank=True,null=True,verbose_name=u'出版日期')
 
@@ -76,6 +77,8 @@ class Actors(models.Model):
 
     def __str__(self):
         return self.name
+    class Meta:
+        ordering=['name']
 
 class Directors(models.Model):
     name = models.CharField(max_length=50,verbose_name=u'导演姓名')
@@ -83,6 +86,8 @@ class Directors(models.Model):
 
     def __str__(self):
         return self.name
+    class Meta:
+        ordering=['name']
 
 class Movies(models.Model):
     '''
@@ -93,6 +98,7 @@ class Movies(models.Model):
     movie_img = models.ImageField(upload_to='photos',verbose_name=u'海报')    #添加电影海报 img 字段
     actor = models.ManyToManyField(Actors,verbose_name=u'演员')
     derector = models.ForeignKey(Directors,verbose_name=u'导演')
+    movie_intro = models.TextField(verbose_name=u'电影介绍')       #添加电影介绍
     movie_length = models.IntegerField(verbose_name=u'时长(分钟)')
     show_time = models.DateTimeField(verbose_name=u'上映时间')
 
@@ -109,6 +115,7 @@ class Programs(models.Model):
     pro_name = models.CharField(max_length=50,verbose_name=u'语言')
     creater = models.CharField(max_length=50,verbose_name=u'创始人')
     create_date = models.DateTimeField(blank=True,null=True,verbose_name=u'创始时间')
+    pro_intro = models.TextField(verbose_name=u'语言介绍')
 
     def __str__(self):
         return self.pro_name
